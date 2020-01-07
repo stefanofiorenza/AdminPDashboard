cf8a7c337eb8329793ec932f5c7d935c50300a20
stefanofiorenza <stefanofiorenza@gmail.com>
2020-01-07 11:54:51 +0200

    [DataTable]: fixed pagination seize dropdown in table-all

--- src/components/pages/tables/TableToolkitPaginationPage.jsx
+++ src/components/pages/tables/TableToolkitPaginationPage.jsx
@@ -19,7 +19,7 @@ import 'react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min.c
 import './TableToolkitPaginationPage.css';
 
 
-import paginationFactory, { PaginationProvider, PaginationListStandalone } from 'react-bootstrap-table2-paginator';
+import paginationFactory, { PaginationProvider, PaginationListStandalone, SizePerPageDropdownStandalone  } from 'react-bootstrap-table2-paginator';
 
 import {productsData} from '../../data/ProductsData.js';
 
@@ -49,6 +49,35 @@ const customTotal = (from, to, size) => (
   </span>
 );
 
+const paginationConfig = {
+  custom: true,
+  paginationSize: 4,
+  pageStartIndex: 1,
+  firstPageText: 'First',
+  prePageText: 'Back',
+  nextPageText: 'Next',
+  lastPageText: 'Last',
+  nextPageTitle: 'First page',
+  prePageTitle: 'Pre page',
+  firstPageTitle: 'Next page',
+  lastPageTitle: 'Last page',
+  showTotal: true,
+  paginationTotalRenderer: customTotal,
+  sizePerPageList: [{
+    text: '10', value: 10
+  }, {
+    text: '25', value: 25
+  }, {
+      text: '50', value: 50
+  }, {
+      text: '100', value: 100
+  }, {
+    text: 'All', value: productsData.length
+  }] // A numeric array is also available. the purpose of above example is custom the text
+};
+
+
+
 export default class TableToolkitPaginationPage extends React.Component{
     
     constructor(props) {
@@ -57,36 +86,22 @@ export default class TableToolkitPaginationPage extends React.Component{
 
     render() {
 
-        const options = {
-            custom: true,
-            paginationSize: 4,
-            pageStartIndex: 1,
-            firstPageText: 'First',
-            prePageText: 'Back',
-            nextPageText: 'Next',
-            lastPageText: 'Last',
-            nextPageTitle: 'First page',
-            prePageTitle: 'Pre page',
-            firstPageTitle: 'Next page',
-            lastPageTitle: 'Last page',
-            showTotal: true,
-            totalSize: productsData.length
-          };
+        const contentTable = ({ paginationProps, paginationTableProps }) => {
 
-        const contentTable = ({ paginationProps, paginationTableProps }) => (
-          
-            <div>            
-              {/* <PaginationListStandalone { ...paginationProps } /> */}
+          return (
+            <div>
+               
               <ToolkitProvider
                 keyField="id"
                 columns={ columns }
                 data={ productsData }
                 search
               >
-                {
-                  toolkitprops => (
-                    <div>
-                
+               {
+                 (toolkitprops)=>{
+
+                  return   (
+                  <div>                
                       <div className="row">     
                           <div className="col-sm-8">
                           </div>             
@@ -94,26 +109,33 @@ export default class TableToolkitPaginationPage extends React.Component{
                             <SearchBar { ...toolkitprops.searchProps } />
                           </div>
                       </div>                                  
-                     <br/>
+                      <br/>
+
                       <BootstrapTable
                         striped
                         hover
                         { ...toolkitprops.baseProps }
                         { ...paginationTableProps }
                       />
-                    </div>
-                  )
-                }
+
+                  </div>);  
+
+
+                 }
+               }
               </ToolkitProvider>
-              <PaginationListStandalone { ...paginationProps } />
+                 
+              <SizePerPageDropdownStandalone { ...paginationProps } />
+              <PaginationListStandalone { ...paginationProps } /> 
             </div>
           );
+        }
           
 
         return (        
             <div>
               <h2>Table Toolkit (Search) and (Pagination) Demo:</h2>
-              <PaginationProvider pagination={paginationFactory(options)} >
+              <PaginationProvider pagination={paginationFactory(paginationConfig)} >
                   { contentTable }
               </PaginationProvider>              
             </div>        
